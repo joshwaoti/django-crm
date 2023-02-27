89fe3dcf9e6b2dc5260ab7b44032925664147507
joshwaoti <joshwaotieno643@gmail.com>
2023-02-27 22:10:41 +0300

    db created

--- mydb.py
+++ mydb.py
@@ -1,9 +1,11 @@
 import mysql.connector
 
+
 dataBase = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    passwd = 'Jeiel9411'
+    passwd = 'Jeiel9411',
+    # auth_plugin='mysql_native_password'
 )
 
 # prepare a cursor object
